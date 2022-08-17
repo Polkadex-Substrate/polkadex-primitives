e67c02a31a9e34f03fb1b88ecab68a12600e2922
Faisal Ahmed <faisalahmedfarooq46@gmail.com>
2022-08-18 01:59:22 +0530

    fix: commenting out ser and deser

--- src/withdrawal.rs
+++ src/withdrawal.rs
@@ -1,10 +1,13 @@
 use crate::assets::AssetId;
 use codec::{Decode, Encode, MaxEncodedLen};
 
+/* #[cfg(feature = "std")]
+use serde::{Serialize, Deserialize}; */
+
 use scale_info::TypeInfo;
 
 #[derive(Clone, Encode, Decode, MaxEncodedLen, TypeInfo, Debug, PartialEq)]
-#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+// #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 pub struct Withdrawal<AccountId, Balance> {
     pub main_account: AccountId,
     pub amount: Balance,

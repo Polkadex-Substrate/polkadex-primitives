311c3cb56ddb09b1b852ff6747119d2bb8f0c8bc
Gautham <gauthamastro@gmail.com>
2022-08-22 20:46:05 +0530

    Add Derserialize, Serialize to EnclaveSnapshot

--- src/snapshot.rs
+++ src/snapshot.rs
@@ -38,7 +38,6 @@ pub struct Fees<Balance: Zero + Clone>{
 
 #[derive(Clone, Encode, Decode, MaxEncodedLen, TypeInfo)]
 #[cfg_attr(feature = "std",derive(Debug))]
-// #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
 #[scale_info(skip_type_params(SnapshotAccLimit, WithdrawalLimit,AssetsLimit ))]
 pub struct EnclaveSnapshot<Account: Ord, Balance: Zero + Clone, WithdrawalLimit: Get<u32>, AssetsLimit: Get<u32>, SnapshotAccLimit: Get<u32>> {
     /// Serial number of snapshot.
@@ -47,6 +46,40 @@ pub struct EnclaveSnapshot<Account: Ord, Balance: Zero + Clone, WithdrawalLimit:
     pub merkle_root: H256,
     /// Withdrawals
     pub withdrawals: BoundedBTreeMap<Account, BoundedVec<Withdrawal<Account, Balance>, WithdrawalLimit>, SnapshotAccLimit>,
+    /// Fees collected by the operator
+    pub fees: BoundedVec<Fees<Balance>,AssetsLimit>
+}
+
+#[cfg(feature = "std")]
+impl<Account: Ord,
+    Balance: Zero + Clone,
+    WithdrawalLimit: Get<u32>,
+    AssetsLimit: Get<u32>,
+    SnapshotAccLimit: Get<u32>
+> TryFrom<EnclaveSnapshotStd<Account, Balance, WithdrawalLimit, AssetsLimit>> for
+EnclaveSnapshot<Account, Balance, WithdrawalLimit, AssetsLimit, SnapshotAccLimit> {
+    type Error = ();
+
+    fn try_from(value: EnclaveSnapshotStd<Account, Balance, WithdrawalLimit, AssetsLimit>) -> Result<Self, Self::Error> {
+        Ok(EnclaveSnapshot {
+            snapshot_number: value.snapshot_number,
+            merkle_root: value.merkle_root,
+            withdrawals: BoundedBTreeMap::try_from(value.withdrawals)?,
+            fees: value.fees
+        })
+    }
+}
+
+/// This is for use passing data from enclave to relayer via RPC, it's short cut.
+#[cfg_attr(feature = "std",derive(Deserialize,Serialize, Debug))]
+#[cfg(feature = "std")]
+pub struct EnclaveSnapshotStd<Account: Ord, Balance: Zero + Clone, WithdrawalLimit: Get<u32>, AssetsLimit: Get<u32>> {
+    /// Serial number of snapshot.
+    pub snapshot_number: u32,
+    /// Hash of the balance snapshot dump made by enclave. ( dump contains all the accounts in enclave )
+    pub merkle_root: H256,
+    /// Withdrawals
+    pub withdrawals: BTreeMap<Account, BoundedVec<Withdrawal<Account, Balance>, WithdrawalLimit>>,
     /// Fees collected by the operator
     pub fees: BoundedVec<Fees<Balance>,AssetsLimit>
 }

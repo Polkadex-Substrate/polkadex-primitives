9cf18dfb4b9fe8f2c6d16f03947aebd398720388
zktony <zktony@protonmail.com>
2023-02-28 12:42:58 +0530

    Added Snapshot Summary

--- src/withdrawal.rs
+++ src/withdrawal.rs
@@ -5,6 +5,8 @@ use scale_info::TypeInfo;
 
 #[cfg(feature = "std")]
 use serde::{Deserialize, Serialize};
+use sp_core::H256;
+use crate::{AccountId, BlockNumber, Header};
 
 #[derive(Clone, Encode, Decode, MaxEncodedLen, TypeInfo, Debug, PartialEq)]
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
@@ -14,4 +16,32 @@ pub struct Withdrawal<AccountId> {
     pub asset: AssetId,
     pub event_id: u64,
     pub fees: Decimal,
+}
+
+#[derive(Clone, Encode, Decode, MaxEncodedLen, TypeInfo, Debug, PartialEq)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub struct WithdrawalPayload {
+    pub asset_id: AssetId,
+    pub amount: Decimal,
+    pub user: AccountId,
+}
+
+#[derive(Encode, Decode, Serialize, Deserialize, Debug, Clone)]
+pub struct Withdrawals {
+    pub withdrawals: Vec<WithdrawalPayload>,
+    pub nonce: u32,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug, Encode, Decode)]
+pub struct SnapshotSummary {
+    // Last synced blocknumber
+    pub last_block: BlockNumber,
+    // Snapshot Number
+    pub snapshot_number: u64,
+    // Hash of the enclave state
+    pub enclave_state_hash: H256,
+    // The header that was used to initialise the enclave
+    pub initialization_header: Header,
+    // Pending Withdrawals
+    pub withdrawals_processed: Withdrawals,
 }
